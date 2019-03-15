c876073f3ba8b92f1d2bb97ef34a244b1ae19c5e
Admin <Admin@192.168.131.1>
2019-03-15 18:38:13 +0530

    hii how r you

--- github/src/github/Hello.java
+++ github/src/github/Hello.java
@@ -2,6 +2,6 @@ package github;
 
 public class Hello {
 public static void main(String[] args) {
-	System.out.println("hello india , hii how are you");
+	System.out.println("hello india , hii how r u");
 }
 }
